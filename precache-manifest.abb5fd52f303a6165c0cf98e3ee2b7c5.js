7b6bca8ea851a2c52285e204240cded28863a166
Maciej Gdula <94039553+MG9944@users.noreply.github.com>
2022-04-06 12:39:42 +0200

    Updates

---
+++ precache-manifest.abb5fd52f303a6165c0cf98e3ee2b7c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d929e1d4122b58dd00fc140367e43cbb",
+    "url": "/BudgetApp/index.html"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/BudgetApp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/BudgetApp/static/js/2.360a4eba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BudgetApp/static/js/2.360a4eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b88d40f5b32640d2c23",
+    "url": "/BudgetApp/static/js/main.ad1454a9.chunk.js"
+  },
+  {
+    "revision": "96303c80703495aba7b2",
+    "url": "/BudgetApp/static/js/runtime-main.c8f97508.js"
+  }
+]);
